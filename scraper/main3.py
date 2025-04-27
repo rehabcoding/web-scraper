ef96788a26f99aa66802761b9ce5fbe34d61478b
Rehab Tasawar <111608024+rehabcoding@users.noreply.github.com>
2025-04-27 18:37:21 +0000

    Loop commit

--- scraper/main3.py
+++ scraper/main3.py
@@ -3,6 +3,9 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Put in the skill that you are not familiar with")
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 html_text = requests.get('https://m.timesjobs.com/mobile/jobs-search-result.html?txtKeywords=python&cboWorkExp1=-1&txtLocation=').text
 # print(html_text) # 200 means request accepted
 soup = BeautifulSoup(html_text, 'lxml')
@@ -15,8 +18,8 @@ for job in jobs:
     if '8' in job_published:
         company_name = job.find('span', class_='srp-comp-name').text
         skills = job.find('div', class_='srp-keyskills').text
-
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
+        more_info = job.div.a['href']
+        if unfamiliar_skill not in skills:
+            print(f"Company Name: {company_name}")
+            print(f"Required Skills: {skills.strip()}\n")
+            print(f'More Information: {more_info}')
